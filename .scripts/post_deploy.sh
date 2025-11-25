3bc89ccf2fe37fcf47885acf5ebdcf93829b51cf
Bobsans <mr.bobsans@gmail.com>
2025-11-25 20:32:53 +0300

    change workflow script

--- .scripts/post_deploy.sh
+++
@@ -1,9 +0,0 @@
-OUT=${1%/}
-cp "$OUT/config.yaml" /tmp/sigpc/
-rm -rf "$OUT"
-mv /tmp/sigpc "$OUT"
-python3.14 -m venv "$OUT/.venv"
-cd "$OUT" || exit
-source "$OUT/.venv/bin/activate"
-pip install -r requirements.txt
-rm -r "$OUT/.scripts"
